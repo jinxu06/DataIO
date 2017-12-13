8d53211791c7df75e7385ede40cd5ed7d2fb2465
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-13 22:39:16 +0000

    update

--- mnist.py
+++ mnist.py
@@ -2,11 +2,14 @@ import numpy as np
 import os
 import pprint
 
+from data_provider import DataProvider
+
 DATA_DIR = "/data/ziz/not-backed-up/datasets-ziz-only"
 import pprint
 
 def info():
     meta_dict = {
+        "name": "mnist",
         "location": os.path.join(DATA_DIR, "raw_data/mnist", "mnist.npz"),
         "available set": ['train', 'test'],
         "available mode": ['all', 'batch'],
@@ -29,5 +32,20 @@ def _load_data_all(path, which_set):
     data = np.load(path)
     return data["x_"+which_set], data['y_'+which_set]
 
-def _load_data_batch():
+def _load_data_batch(path, which_set, batch_size):
     pass
+
+class MNISTIterator(DataProvider):
+
+    def __init__(self, which_set="train", batch_size=100, max_num_batches=-1, shuffle_order=True):
+
+        inputs, targets = load_data(which_set, "all")
+        super().__init__(
+            inputs, targets, batch_size, max_num_batches, shuffle_order, rng=None)
+
+    def next(self):
+        """Returns next data batch or raises `StopIteration` if at end."""
+        inputs_batch, targets_batch = super(MNISTDataProvider, self).next()
+        return inputs_batch, targets_batch
+
+    
