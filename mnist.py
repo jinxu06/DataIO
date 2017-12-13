4296dd6f4745fc784abd4f292fe06ff189816b40
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-13 21:53:20 +0000

    update

--- mnist.py
+++ mnist.py
@@ -1,4 +1,22 @@
 import numpy as np
+import os
 
-def load_data():
+DATA_DIR = "/data/ziz/not-backed-up/dataset-ziz-only"
+
+def load_data(which_set="train", mode="all"):
+    assert which_set in ['train', 'valid', 'test'],
+            "which_set takes value train, valid, test"
+    assert mode in ['all', 'batch'],
+            "mode takes value all, batch"
+    if mode == 'all':
+        path = os.path.join(DATA_DIR, "raw_data/mnist-{0}.npz".format(which_set))
+        return load_data_all(path)
+    elif mode == 'batch':
+        return load_data_batch()
+
+def load_data_all(path):
+    data = np.load(path)
+    return data
+
+def load_data_batch():
     pass
