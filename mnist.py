b362d497f6f73dd7fc79e6827c5d3f2222f46354
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-13 22:26:12 +0000

    update

--- mnist.py
+++ mnist.py
@@ -1,11 +1,22 @@
 import numpy as np
 import os
+import pprint
 
 DATA_DIR = "/data/ziz/not-backed-up/datasets-ziz-only"
+import pprint
+
+def info():
+    meta_dict = {
+        "location": os.path.join(DATA_DIR, "raw_data/mnist", "mnist.npz"),
+        "available set": ['train', 'test'],
+        "available mode": ['all', 'batch'],
+    }
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(meta_dict)
 
 def load_data(which_set="train", mode="all"):
     assert which_set in ['train', 'valid', 'test'], \
-            "which_set takes values in [train, valid, test]"
+            "which_set takes values in [train, test]"
     assert mode in ['all', 'batch'], \
             "mode takes values in [all, batch]"
     if mode == 'all':
