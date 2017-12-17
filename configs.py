2368fd15d930a77ce3147c106a489eaaa2b84110
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-17 10:42:11 +0000

    update

---
+++ configs.py
@@ -0,0 +1,6 @@
+DATA_DIRS = [
+    "/data/ziz/not-backed-up/datasets-ziz-only/raw_data",
+    "/data/ziz/not-backed-up/datasets-ziz-only/processed_data",
+    "/data/ziz/not-backed-up/datasets-ziz-all/raw_data",
+    "/data/ziz/not-backed-up/datasets-ziz-all/processed_data",
+]
---
+++ test.py
@@ -0,0 +1,4 @@
+from utils.io import inspect_data_dirs
+from configs import DATA_DIRS
+
+inspect_data_dirs(DATA_DIRS)
