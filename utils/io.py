58e718be8cff1aabbb42c660dcb1f930ef8b803f
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-17 11:14:40 +0000

    update

--- utils/io.py
+++ utils/io.py
@@ -14,3 +14,14 @@ def read_images_batch(path, batch_size, suffix=""):
     fs = filter(lambda x: x.endswith(suffix), filenames)
     fs = sorted(fs)
     return np.array([np.array(Image.open(os.path.join(dirpath, fs[i]))) for i in range(batch_size)])
+
+
+def inspect_data_dirs(dirs):
+    print("Inspect data folders......")
+    for d in dirs:
+        if os.path.exists(d):
+            dirpath, dirnames, filenames = next(os.walk(d))
+            if len(dirnames)+len(filenames) ==0:
+                print("{0}: folder exists but empty".format(d))
+        else:
+            print("{0}: folder not found or not accessible".format(d))
